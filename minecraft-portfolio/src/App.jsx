296e7154ed3c548f02a7df3e802234627479899e
Eva <erosati@uwaterloo.ca>
2026-01-03 16:39:58 -0500

    created scene, camera and renderer

--- minecraft-portfolio/src/App.jsx
+++ minecraft-portfolio/src/App.jsx
@@ -1,35 +1,30 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import {useEffect} from 'react'; // handle side effects and rendering
+import * as THREE from 'three'; // import to use 3D objects and scenes
 
-function App() {
-  const [count, setCount] = useState(0)
+function App(){ // container for entire site
+  useEffect(()=>{
+    // create scene with objects
+    const scene = new THREE.Scene(); 
+    // create camera -> viewpoint
+    const camera = new THREE.PerspectiveCamera( // acts like eyes, need this in order to see the scene
+      75, // field of view
+      window.innerWidth / window.innerHeight, // aspect ratio
+      0.1, // regulate object size
+      1000
+    );
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    camera.position.z = 5; // move camera along z axis to view the object from behind
+
+    // create a renderer to display the scene 
+    const renderer = new THREE.WebGLRenderer();
+    renderer.setSize( window.innerWidth, window.innerHeight ); // canvas size
+    document.body.appendChild( renderer.domElement ); // add to webpage
+    
+
+  })
+
+  return <div />;
+
 }
 
-export default App
+export default App;
